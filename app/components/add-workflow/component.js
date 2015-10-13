5018ff61864482377eb446918c95359da9577e6a
Westly Wright <westly.wright@gmail.com>
2015-10-12 17:11:37 -0700

    add dragula

--- app/components/add-workflow/component.js
+++ app/components/add-workflow/component.js
@@ -3,25 +3,48 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     selectChange(index) {
-      if (index === (this.get('serviceFunctions').length - 1)) {
-        Ember.run(() => {
-          this.get('serviceFunctions').pushObject({value: ''});
-        });
+        if (index === (this.get('serviceFunctions').length - 1)) {
+          Ember.run(() => {
+            this.get('serviceFunctions').pushObject({
+              value: ''
+            });
+          });
+        }
+      },
+      cancel() {
+        this.sendAction('dismiss');
+      },
+      dragulaEvent() {
+        console.log('yeah bitch');
       }
-    },
-    cancel() {
-      this.sendAction('dismiss');
-    }
   },
-  serviceFunctions: [
-    {
-      value: ''
+  serviceFunctions: [{
+    value: ''
+  }, {
+    value: ''
+  }, {
+    value: ''
+  }],
+  'dragulaconfig': {
+    'options': {
+      copy: false,
+      revertOnSpill: false,
+      removeOnSpill: false,
+      ignoreInputTextSelection: true,
+      moves: function(el, source, handle, sibling) {
+        if ($(handle).hasClass('fa-bars')) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      //Other options from the dragula source page.
     },
-    {
-      value: ''
-    },
-    {
-      value: ''
-    }
-  ]
+    'eventList': [{
+        name: 'drag'
+      }, {
+        name: 'drop'
+      }] // all the events that you want to listen to. TBD - will make this simpler.
+  }
 });
