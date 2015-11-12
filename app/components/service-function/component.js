ce0d40934b9194da48bd6062dd681c8cfe51a821
Westly Wright <westly.wright@gmail.com>
2015-11-12 15:15:08 -0700

    more fixes

--- app/components/service-function/component.js
+++ app/components/service-function/component.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 import C from 'ui/utils/constants';
 
 export default Ember.Component.extend({
+  containers: null,
   actions: {
     toggleCollapse() {
       if (this.get('containers')) {
-          this.toggleProperty('collapsed');
+        this.toggleProperty('collapsed');
       } else {
         var serviceName = this.get('function.name');
         Ember.$.ajax(`/v1/proxy/${C.HAUWEI.API_ENDPOINT}/serviceFunction/view?name=${serviceName}`, 'GET').then((response) => {
@@ -16,7 +17,17 @@ export default Ember.Component.extend({
       }
     }
   },
-  collapsed: true,
+  willInsertElement: function() {
+    var serviceName = this.get('function.name');
+    Ember.$.ajax(`/v1/proxy/${C.HAUWEI.API_ENDPOINT}/serviceFunction/view?name=${serviceName}`, 'GET').then((response) => {
+      if (!response.containers.length) {
+        this.set('collapsed', true);
+      }
+      this.set('containers', response.containers);
+      return response;
+    }, function( /*error*/ ) {});
+  },
+  collapsed: false,
   imageEndpoint: Ember.computed('C.HAUWEI.API_ENDPOINT', function() {
     return C.HAUWEI.API_ENDPOINT;
   }),
